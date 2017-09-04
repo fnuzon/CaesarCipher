c88892143ec519ada7cd4e1159bb3adced2d086d
fnuzon <nikotamminen2@hotmail.com>
2017-09-04 19:52:30 +0300

    Add project files.

---
+++ CaesarCipher/Algorithm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Algorithm
+    {
+        public const string a = "abcdefghijklmnopqrstuvwxyz";
+        public static int forward = 0;
+        
+        public static int ChangeKey
+        {
+            get {
+
+                return forward;
+
+                }
+            set {
+
+                forward = value;
+
+                }
+        }
+        public static char forwardAlphabet(char i)
+        {
+            var pointer = a.IndexOf(i);
+            if (pointer == -1)
+            {
+                return i;
+            }
+            pointer = ((pointer + forward) + a.Length) % a.Length;
+            return a[pointer];
+
+        }
+        public static string stringAlphabet(string x)
+        {
+            return new string(x.Select(forwardAlphabet).ToArray());
+        }
+
+        public static void decrypt(string x)
+        {
+            ChangeKey = (forward - 2 * forward);
+            var decryptedText = stringAlphabet(x);
+            Console.WriteLine("Decrypted text is:" + decryptedText);
+        }
+    }
+}
---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int input;
+            string text;
+            string encryptedText;
+            int cycle = 0;           
+            do
+            {
+
+               
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("1. to enter text to encrypt");
+                Console.WriteLine("2. to cycle alphabets");
+                Console.WriteLine("3. decrypt text");
+                input = int.Parse(Console.ReadLine());
+                if (input == 1)
+                {
+                    Console.WriteLine("Give text: ");
+                    text = Console.ReadLine();
+                    encryptedText = Algorithm.stringAlphabet(text);
+                    Console.WriteLine("Encrypted text: ");
+                    Console.WriteLine(encryptedText);
+                }
+                if (input == 2)
+                {
+                    Console.WriteLine("Give value how much to cycle alphabets");
+                    cycle = int.Parse(Console.ReadLine());                    
+                    Algorithm.ChangeKey = cycle;
+                }
+                if (input == 3)
+                {
+                    Console.WriteLine("Give text to decrypt");
+                    text = Console.ReadLine();
+                    Algorithm.decrypt(text);
+                }
+            }
+            while (input != 0);
+
+            
+        }
+    }
+}
